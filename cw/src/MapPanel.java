73b6041a99e136f8559d4d2a0e7bee5c47e21ab2
andiesantos <amsantos12@up.edu.ph>
2017-11-15 16:05:24 +0800

    Updated some map boundaries

--- cw/src/MapPanel.java
+++ cw/src/MapPanel.java
@@ -8,13 +8,25 @@ import java.awt.Toolkit;
 
 public class MapPanel extends JPanel {
 	Rectangle redBase;
+	Rectangle water;
+	Rectangle blueBase;
 
 	public void paintComponent(Graphics g) {
 		Graphics2D g2d = (Graphics2D)g;
 
-		redBase = new Rectangle(100, 100, 100, 100);
+		redBase = new Rectangle(0, 0, 1100, 150);
 			// put constants in last 2 params
 		g2d.setColor(Color.RED);
 		g2d.fill(redBase);
+
+		water = new Rectangle(0, 150, 1100, 200);
+			// put constants in last 2 params
+		g2d.setColor(Color.BLUE);
+		g2d.fill(water);
+
+		blueBase = new Rectangle(0, 350, 1100, 150);
+			// put constants in last 2 params
+		g2d.setColor(Color.GREEN);
+		g2d.fill(blueBase);
 	}
 }
